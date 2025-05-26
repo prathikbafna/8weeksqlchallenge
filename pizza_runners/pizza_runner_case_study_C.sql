22530c5fa035451834e1eada4425d365d8e9253f
prathikbafna <44114369+prathikbafna@users.noreply.github.com>
2025-05-25 22:46:19 -0600

    combined detias of order items, add-ons, exludes

--- pizza_runners/pizza_runner_case_study_C.sql
+++ pizza_runners/pizza_runner_case_study_C.sql
@@ -3,7 +3,6 @@
 -- What are the standard ingredients for each pizza?
 use pizza_runner;
 
-
 select *, concat(' ', topping_id) as updated from pizza_recipes
 cross join pizza_toppings
 where locate(concat(' ', topping_id) ,toppings) > 0;
@@ -11,10 +10,57 @@ where locate(concat(' ', topping_id) ,toppings) > 0;
 
 -- What was the most commonly added extra?
 
-select * from customer_orders;
+use pizza_runner;
+
+WITH RECURSIVE split_cte AS (
+    SELECT
+        order_id,
+        SUBSTRING_INDEX(extras, ',', 1) AS item,
+        SUBSTRING(extras, LENGTH(SUBSTRING_INDEX(extras, ',', 1)) + 2) AS rest
+    FROM customer_orders
+
+    UNION ALL
+
+    SELECT
+        order_id,
+        SUBSTRING_INDEX(rest, ',', 1),
+        SUBSTRING(rest, LENGTH(SUBSTRING_INDEX(rest, ',', 1)) + 2)
+    FROM split_cte
+    WHERE rest != ''
+)
+
+select item as extras, count(item) as num_of_times 
+from split_cte
+where item is not null and item <> 'null' and item <> ''
+group by item
+order by 2 desc;
 
 
 -- What was the most common exclusion?
+
+use pizza_runner;
+
+with recursive temp as (
+select 
+	order_id,
+    substring_index(exclusions,',',1) as item,
+    substring(exclusions, length(exclusions) + 2) as rest
+from customer_orders
+
+union all
+
+select
+	order_id,
+    substring_index(rest,',',1),
+    substring(rest, length(rest) + 2)
+from temp
+where rest <> ''
+)
+
+select item as topping_id, count(item) as num_of_time_excluded from temp
+where item is not null and item <> 'null' and item <> ''
+group by item
+order by 2 desc;
 
 
 -- Generate an order item for each record in the customers_orders table in the format of one of the following:
@@ -22,6 +68,71 @@ select * from customer_orders;
 -- Meat Lovers - Exclude Beef
 -- Meat Lovers - Extra Bacon
 -- Meat Lovers - Exclude Cheese, Bacon - Extra Mushroom, Peppers
+
+
+with recursive 
+customer_orders_details as (
+	select c.order_id, c.pizza_id, p.pizza_name, c.extras, c.exclusions
+	from customer_orders c join pizza_names as p
+	on p.pizza_id = c.pizza_id
+),
+excluded_details as (
+	select 
+		order_id,
+        pizza_name,
+		substring_index(exclusions,',',1) as item,
+		substring(exclusions, length(exclusions) + 2) as rest
+	from customer_orders_details
+
+	union all
+
+	select
+		order_id,
+        pizza_name,
+		substring_index(rest,',',1),
+		substring(rest, length(rest) + 2)
+	from excluded_details
+	where rest <> ''
+),
+extras_details AS (
+    SELECT
+		order_id,
+        pizza_name,
+        SUBSTRING_INDEX(extras, ',', 1) AS item,
+        SUBSTRING(extras, LENGTH(SUBSTRING_INDEX(extras, ',', 1)) + 2) AS rest
+    FROM customer_orders_details
+
+    UNION ALL
+
+    SELECT
+		order_id,
+        pizza_name,
+        SUBSTRING_INDEX(rest, ',', 1),
+        SUBSTRING(rest, LENGTH(SUBSTRING_INDEX(rest, ',', 1)) + 2)
+    FROM extras_details
+    WHERE rest != ''
+),
+detailed_names as (
+select e.order_id, e.pizza_name, concat(e.pizza_name, ' - Exclude ',group_concat(p.topping_name order by p.topping_name separator ', ')) as items
+from excluded_details e 
+join pizza_toppings p on e.item = p.topping_id
+where item is not null and item <> 'null' and item <> ''
+group by 1,2
+
+union all
+
+select e.order_id, e.pizza_name, concat('- Extra ',group_concat(p.topping_name order by p.topping_name separator ', ')) as items
+from extras_details e 
+join pizza_toppings p on e.item = p.topping_id
+where item is not null and item <> 'null' and item <> ''
+group by 1,2)
+
+select group_concat(items order by items separator ' ')  as order_details
+from detailed_names
+group by order_id, pizza_name;
+
+
+-- select * from detailed_names;
 
 
 -- Generate an alphabetically ordered comma separated ingredient list for each pizza order from the customer_orders table and add a 2x in front of any relevant ingredients
